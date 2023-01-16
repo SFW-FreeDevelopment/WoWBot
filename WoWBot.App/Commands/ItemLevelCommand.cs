b7c0e2a21e961781ae43f2eef07de1b72172e9ee
Steven Garberg <steven.garberg@riviahealth.com>
2023-01-16 15:30:32 -0500

    Added level and item level commands

---
+++ WoWBot.App/Commands/ItemLevelCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using ReminderBot.App.Services;
+
+namespace ReminderBot.App.Commands;
+
+public class ItemLevelCommand : CommandBase
+{
+    private readonly ICharacterService _characterService;
+    
+    public ItemLevelCommand(ICharacterService characterService)
+    {
+        _characterService = characterService;
+    }
+    
+    [Command("ilvl")]
+    public async Task HandleCommandAsync(string character, [Remainder] string realm)
+    {
+        var level = await _characterService.GetAverageItemLevel(realm, character);
+        await ReplyAsync($"{character}'s average equipped item level is {level}.");
+    }
+}
---
+++ WoWBot.App/Commands/LevelCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using ReminderBot.App.Services;
+
+namespace ReminderBot.App.Commands;
+
+public class LevelCommand : CommandBase
+{
+    private readonly ICharacterService _characterService;
+    
+    public LevelCommand(ICharacterService characterService)
+    {
+        _characterService = characterService;
+    }
+    
+    [Command("level")]
+    public async Task HandleCommandAsync(string character, [Remainder] string realm)
+    {
+        var profile = await _characterService.GetCharacterProfile(realm, character);
+        await ReplyAsync($"{character}'s level is {profile.Level}.");
+    }
+}
