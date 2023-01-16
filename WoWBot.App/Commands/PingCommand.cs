1490b14fb78bfc45023e4bd046100a1c6d32109f
Steven Garberg <steven.garberg@riviahealth.com>
2023-01-16 00:14:10 -0500

    Setup bot

---
+++ WoWBot.App/Commands/PingCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace ReminderBot.App.Commands;
+
+public class PingCommand : CommandBase
+{
+    [Command("ping")]
+    public async Task HandleCommandAsync()
+    {
+        await ReplyAsync("I am pinging the server.");
+    }
+}
--- WoWBot.App/Program.cs
+++ WoWBot.App/Program.cs
@@ -1,3 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+namespace ReminderBot.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunBotAsync().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static DiscordSocketClient _client;
+        private static CommandService _commands;
+        private static IServiceProvider _services;
+
+        public static async Task RunBotAsync()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<IConfiguration>(_ => configuration)
+                .BuildServiceProvider();
+
+            //new ReminderService(reminderRepository).CheckReminders().SafeFireAndForget();
+
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, configuration["DiscordBotToken"]);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+        
+        private static async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private static async Task HandleCommandAsync(SocketMessage socketMessage)
+        {
+            var message = socketMessage as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("wowbot ", ref argPos))
+            {
+                await Execute();
+                return;
+            }
+            argPos = 0;
+            if (message.HasStringPrefix("<@1002374960129507358> ", ref argPos))
+            {
+                await Execute();
+                return;
+            }
+
+            async Task Execute()
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
