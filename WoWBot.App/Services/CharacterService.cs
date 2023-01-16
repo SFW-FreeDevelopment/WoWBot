fcc3378bd6fb69574625db89747c1948af00b46f
Steven Garberg <steven.garberg@riviahealth.com>
2023-01-16 15:30:22 -0500

    Added services

---
+++ WoWBot.App/Services/CharacterService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+using ReminderBot.App.Models;
+
+namespace ReminderBot.App.Services;
+
+public interface ICharacterService
+{
+    Task<GetCharacterProfileSummaryResponse> GetCharacterProfile(string realm, string character);
+    Task<int> GetAverageItemLevel(string realm, string character);
+}
+
+public class CharacterService : ICharacterService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ITokenService _tokenService;
+    
+    public CharacterService(HttpClient httpClient, ITokenService tokenService)
+    {
+        _httpClient = httpClient;
+        _tokenService = tokenService;
+    }
+
+    public async Task<GetCharacterProfileSummaryResponse> GetCharacterProfile(string realm, string character)
+    {
+        realm = HttpUtility.UrlEncode(realm.ToLower());
+        character = HttpUtility.UrlEncode(character.ToLower());
+        
+        var token = await _tokenService.GetToken();
+        
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://us.api.blizzard.com/profile/wow/character/{realm}/{character}?namespace=profile-us&locale=en_US&access_token={token}")
+        };
+        
+        var response = await _httpClient.SendAsync(request);
+        if (!response.IsSuccessStatusCode) return null;
+
+        var contentString = await response.Content.ReadAsStringAsync();
+        
+        var deserializedResponse = JsonSerializer.Deserialize<GetCharacterProfileSummaryResponse>(contentString);
+        return deserializedResponse;
+    }
+
+    public async Task<int> GetAverageItemLevel(string realm, string character)
+    {
+        var characterProfileSummary = await GetCharacterProfile(realm, character);
+        return characterProfileSummary.EquippedItemLevel;
+    }
+}
---
+++ WoWBot.App/Services/TokenService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using ReminderBot.App.Models;
+
+namespace ReminderBot.App.Services;
+
+public interface ITokenService
+{
+    Task<string> GetToken();
+}
+
+public class TokenService : ITokenService
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+    
+    public TokenService(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+    
+    public async Task<string> GetToken()
+    {
+        var username = _configuration["BattleNet:ClientId"];
+        var password = _configuration["BattleNet:ClientSecret"];
+        var authorization = Convert.ToBase64String(Encoding.Default.GetBytes($"{username}:{password}"));
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://oauth.battle.net/token"),
+            Content = new FormUrlEncodedContent(new []
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            })
+        };
+
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authorization);
+        
+        var response = await _httpClient.SendAsync(request);
+        var contentString = await response.Content.ReadAsStringAsync();
+        
+        var deserializedResponse = JsonSerializer.Deserialize<GetTokenResponse>(contentString);
+        return deserializedResponse.AccessToken;
+    }
+}
